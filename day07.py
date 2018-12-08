d2032aa57dde76bec32800a6dab0d80ea36bd7ae
itscomputers <its_computers@protonmail.com>
2018-12-08 13:15:01 -0700

    day 07

--- day07.py
+++ day07.py
@@ -6,16 +6,21 @@ import string
 
 ##############################
 
-class Node:
-
-    def __init__(self, value):
-        self.val = value
-        self.parents = []
-        self.children = []
+alphabet = {x : i+1 for i, x in enumerate(string.ascii_uppercase)}
 
 ##############################
 
-def parse(data):
+class Node:
+
+    def __init__(self, value, delay):
+        self.val = value
+        self.parents = []
+        self.children = []
+        self.delay = (delay != 0) *  alphabet[value] + delay
+
+##############################
+
+def parse(data, delay=0):
     nodes = dict()
     parent_pattern = re.compile(r'[A-Z] must be finished')
     child_pattern = re.compile(r'[A-Z] can begin')
@@ -23,28 +28,20 @@ def parse(data):
         p = parent_pattern.search(line).group().split(' ')[0]
         c = child_pattern.search(line).group().split(' ')[0]
         if p not in nodes:
-            nodes[p] = Node(p)
+            nodes[p] = Node(p, delay)
         if c not in nodes:
-            nodes[c] = Node(c)
+            nodes[c] = Node(c, delay)
         nodes[p].children.append(c)
         nodes[c].parents.append(p)
     return nodes
 
 ##############################
 
-alphabet = {x : i+1 for i, x in enumerate(string.ascii_uppercase)}
-
-##############################
-
 def process(queue, completed, time_elapsed,
-        ready, workers, free_workers, 
-        delay, offset):
+        ready, workers, free_workers):
     while free_workers > 0 and ready != []:
         val = ready.pop(0)
-        if delay:
-            workers.append([alphabet[val] + offset, val])
-        else:
-            workers.append([0, val])
+        workers.append([queue[val].delay, val])
         free_workers -= 1
     workers.sort()
 
@@ -66,7 +63,7 @@ def process(queue, completed, time_elapsed,
 
 ##############################
 
-def order(queue, num_workers=1, delay=False, offset=0):
+def order(queue, num_workers=1, delay=0):
     completed = ''
     workers = []
     free_workers = num_workers
@@ -75,8 +72,7 @@ def order(queue, num_workers=1, delay=False, offset=0):
     while queue != dict():
         queue, completed, time_elapsed, ready, workers, free_workers = \
                 process(queue, completed, time_elapsed,
-                        ready, workers, free_workers, 
-                        delay, offset)
+                        ready, workers, free_workers)
     return completed, time_elapsed 
 
 #############################
@@ -86,10 +82,9 @@ if __name__ == '__main__':
     with open('data/07.txt') as f:
         queue = parse(f.readlines())
     completed, time_elapsed = order(queue)
-    print('ordering for single worker, no delay: {}'.format(completed))
+    print('{} -- {} seconds'.format(completed, time_elapsed))
 
     with open('data/07.txt') as f:
-        queue = parse(f.readlines())
-    completed, time_elapsed = order(queue, num_workers=6, delay=True, offset=60)
-    print('ordering for 6 workers, 60sec delay: {}'.format(completed))
-    print('time elapsed: {} seconds'.format(time_elapsed))
+        queue = parse(f.readlines(), delay=60)
+    completed, time_elapsed = order(queue, num_workers=6, delay=60)
+    print('{} -- {} seconds'.format(completed, time_elapsed))
