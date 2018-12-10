c5b8917072c1b5e467d190845cc41758861d4b33
itscomputers <its_computers@protonmail.com>
2018-12-10 00:36:18 -0700

    day 09

---
+++ day09.py
@@ -0,0 +1,34 @@
+
+#   advent/2018/day09.py
+
+import re
+from collections import defaultdict, deque
+
+##############################
+
+def initialize():
+    return deque([0]), defaultdict(int)
+
+##############################
+
+def game(num_players, max_value):
+    marbles, score = initialize()
+    for i in range(max_value // 23):
+        for j in range(1, 23):
+            marbles.rotate(-2)
+            marbles.appendleft(23 * i + j)
+        marbles.rotate(7)
+        score[(23 * i) % num_players] += 23 * (i + 1) + marbles.popleft()
+    return max(score.values())
+
+##############################
+
+if __name__ == '__main__':
+
+    with open('data/09.txt') as f:
+        s = f.read()
+        num_players = int(re.search(r'\d* players', s).group().split(' ')[0])
+        max_value = int(re.search(r'\d* points', s).group().split(' ')[0])
+
+    print(game(num_players, max_value))
+    print(game(num_players, 100 * max_value))
