0df2282ef5f5cf6a0bb39cb4201e5c405c950bd1
itscomputers <its_computers@protonmail.com>
2018-12-18 16:45:01 -0700

    day 18

---
+++ day18.py
@@ -0,0 +1,104 @@
+
+#   advent/2018/day18.py
+
+from itertools import product
+from collections import Counter, defaultdict
+
+##############################
+
+def parse(data):
+    resources = dict()
+    for y, line in enumerate(data):
+        for x, ch in enumerate(line):
+            resources[(x,y)] = ch
+    return resources
+
+##############################
+
+class Area:
+
+    def __init__(self, resources):
+        self.resources = resources
+        self.cycle = ['.', '|', '#', '.']
+        self.xmax = max(p[0] for p in resources.keys()) + 1
+        self.ymax = max(p[1] for p in resources.keys()) + 1
+
+    ##########################
+
+    def __repr__(self):
+        return '\n'.join(
+            ''.join(
+                self.resources[(x,y)] for x in range(self.xmax)
+            ) for y in range(self.ymax)
+        )
+
+    ##########################
+
+    def nbhd(self, point):
+        x, y = point
+        return [self.resources[(r, s)] for (r, s)\
+                in product(range(x-1, x+2), range(y-1, y+2)) \
+                if 0 <= r < self.xmax \
+                and 0 <= s < self.ymax \
+                and (r, s) != (x, y)]
+
+    ##########################
+
+    def scan(self, point):
+        n = self.nbhd(point)
+        if self.resources[point] == '.':
+            return n.count('|') > 2
+        if self.resources[point] == '|':
+            return n.count('#') > 2
+        if self.resources[point] == '#':
+            return (n.count('#') * n.count('|')) == 0
+
+    ##########################
+
+    def eval(self, point):
+        for i in range(3):
+            if self.resources[point] == self.cycle[i] and self.scan(point):
+                return self.cycle[i+1]
+        return self.resources[point]
+
+    ##########################
+
+    def advance(self):
+        new_resources = {point : self.eval(point) for point in self.resources}
+        self.resources = new_resources
+
+    ##########################
+
+    def status(self):
+        return Counter(self.resources.values())
+
+    ##########################
+
+    def value(self):
+        return self.status()['|'] * self.status()['#']
+
+##############################
+
+def find_repeat(area):
+    resources = []
+    for i in range(1000):
+        if area.resources in resources:
+            return resources.index(area.resources), i
+        resources.append(area.resources)
+        area.advance()
+
+##############################
+
+if __name__ == '__main__':
+
+    with open('data/18.txt') as f:
+        area = Area(parse([x.rstrip() for x in f.readlines()]))
+
+    for i in range(10):
+        area.advance()
+    print(area.value())
+
+    i0, i1 = find_repeat(area)
+    for j in range((10**9 - i1 - 10) % (i1 - i0)):
+        area.advance()
+    print(area.value())
